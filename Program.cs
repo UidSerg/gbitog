0da4a49b8583b13aa44c67a846f1bc607b7960fa
Uidserg <uidserg@live.com>
2023-03-29 19:08:54 +0500

    готово

--- Program.cs
+++ Program.cs
@@ -1,33 +1,29 @@
 ﻿Console.Write("Введите через запятую элементы массива: ");
-string[] elementArray = Console.ReadLine().Split(",");
+string[] elementArray = Console.ReadLine().Split(",", StringSplitOptions.RemoveEmptyEntries);
+
 /// <summary>
-/// метод формирует массив элементов длина которых не превышает 3 символа из входящего массива
+/// метод формирует массив элементов длина которых не превышает N символа из входящего массива
 /// </summary>
 /// <param name="Array">входящий массив</param>
+/// <param name="N">колличество символов</param>
 /// <returns>итоговый массив</returns>
-string [] LengthArray (string [] Array)
+string [] LengthArray (string [] Array, int N)
 {
 int size = elementArray.Length;
 int newSize = 0;
 for (int i = 0; i < size; i++)
 {
-    if (elementArray[i].Length <= 3)
+    if (elementArray[i].Length <= N)
     {
         newSize++;
     }
 }
-/*
-if (newSize == 0)
-{
-   Console.WriteLine($"в массиве [{String.Join(",", elementArray)}] нет элементов короче 3х символов"); 
-   return;
-}*/
 
 string[] resultArray = new string[newSize];
-int j = 0;
+int j = 0; //счетчик для заполнения нового массива
 for (int i = 0; i < size; i++)
 {
-    if (elementArray[i].Length <= 3)
+    if (elementArray[i].Length <= N )
     {
         resultArray[j] = elementArray[i];
         j++;
@@ -36,6 +32,7 @@ for (int i = 0; i < size; i++)
 }
 return resultArray;
 }
-
-string [] resultArray = LengthArray(elementArray);
-Console.WriteLine($"[{String.Join(",", elementArray)}] -> [{String.Join(",", resultArray)}]"); 
+int N = 3; //длина элента для нового массива
+string [] resultArray = LengthArray(elementArray, N);
+if(resultArray.Length == 0) Console.WriteLine($"[{String.Join(",", elementArray)}] -> в массиве нет элементов длиной меньше {N} символов");
+else Console.WriteLine($"[{String.Join(",", elementArray)}] -> [{String.Join(",", resultArray)}]");
